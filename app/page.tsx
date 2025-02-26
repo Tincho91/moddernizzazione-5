4e7ce7a271d2b3261d6988161a75a7e74f6b6412
Martin Maiaru <tincho_91_15@hotmail.com>
2025-02-26 17:58:48 +0100

    Update Color

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-import Hero from './components/hero'
 import FounderSection from './components/FounderSection'
 import ServicesSection from './components/ServicesSection'
 import InfoSection from './components/InfoSection'
+import Hero from './components/Hero'
 
 
 const Home = () => {
